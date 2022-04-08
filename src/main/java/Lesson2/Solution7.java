6b05b924b1c1e2c8af9ef121d3b5ce9290012415
RKovalevskyi <kovalevskyi.roma@gmail..com>
2022-04-08 18:36:12 +0300

    Lesson2

---
+++ src/main/java/Lesson2/Solution7.java
@@ -0,0 +1,20 @@
+package Lesson2;
+
+import java.util.Arrays;
+
+public class Solution7 {
+    public Solution7() {
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array.length; ++i) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
